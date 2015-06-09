8a3e713bab2847099f7d2c228729312a741b8561
cpoff <curt.poff@gmail.com>
2015-06-09 15:37:41 -0700

    Added email verification

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var app = require('../app')
 var knexConfig = require('../knexfile.js');
 var knex = require('knex')(knexConfig);
-var redis = require("redis"),
-    cache = redis.createClient();
+var uuid = require('node-uuid');
+var nodemailer = require('nodemailer');
+var nonce = uuid.v4();
 
-cache.del('tweets');
-
+usersToAdd = [];
+console.log(nonce);
 // --------------------
 // BUILD INDEX PAGE
 // --------------------
@@ -17,48 +18,22 @@ router.get('/', function(request, response, next) {
         username = request.cookies.username;
         database = app.get('database');
         database('tweets').select().then(function(retreivePosts) {
-            // -----------------------
-            // BEGIN REDIS INSTALL
-            // -----------------------
-            cache.lpush('tweets', JSON.stringify({name: 'username', twit:'tweet', date: 'dateTime'}));
-
-            cache.lrange('tweets', 0, -1, function(err, results) {
-                results.forEach(function(it) {
-                    it = JSON.parse(it);
-                console.log("You're in the loop");
-                console.log(results[0]);
-                                })
-
-                  if (results.length < 1) {
-                    console.log("You're now in the db");
-                       knex('tweets').select().then(function(results) {
-                        console.log('Writing tweets to cache');
-                            
-                            response.render('index', {
-                                tweets: results
-                            });
-                        })
-                    }
-            });
-                retreivePosts.sort(function(a, b) {
-                    console.log("Welcome to the sort.");
-                    if (a.post_number > b.post_number) {
-                        return -1;
-                    }
-                    if (a.post_number < b.post_number) {
-                        return 1;
-                    }
-                    else {
-                        return 0;
-                    }
-                })
-                response.render('index', {
-                    username: username,
-                    tweets: retreivePosts
-                });
+            retreivePosts.sort(function(a, b) {
+                if (a.post_number < b.post_number) {
+                    return -1;
+                }
+                if (a.post_number > b.post_number) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             })
-    }
-    else {
+            response.render('index', {
+                username: username,
+                tweets: retreivePosts
+            });
+        })
+    } else {
         username = null;
         response.render('index', {
             title: "Let's do this",
@@ -71,48 +46,91 @@ router.get('/', function(request, response, next) {
 // --------------------
 router.post('/register', function(request, response) {
     var username = request.body.username,
+        email = request.body.email,
         password = request.body.password,
         password_confirm = request.body.password_confirm,
         database = app.get('database');
-    database('users')
-        .where({
-            'username': username
-        })
-        .then(function(records) {
-            if (records.length > 0) {
-                response.render('index', {
-                    user: null,
-                    error: "Please complete all the fields."
-                })
-                return;
-            }
-            if (!password || !username) {
-                response.render('index', {
-                    user: null,
-                    error: "Please fill out the form completely"
-                });
-                return;
-            }
-            if (password === password_confirm) {
-                console.log('Yes!');
-                database('users')
-                    .insert({
-                        username: username,
-                        password: password,
-                    })
-                    .then(function() {
-                        response.cookie('username', username)
-                        response.redirect('/');
-                    });
-                return;
-            }
-            else {
-                response.render('index', {
-                    error: "Bad pwd, fool."
-                })
-            }
-        })
+    database('users').where({
+        'username': username
+    }).then(function(records) {
+        if (records.length > 0) {
+            response.render('index', {
+                user: null,
+                error: "Please complete all the fields."
+            })
+            return;
+        }
+        if (!password || !username) {
+            response.render('index', {
+                user: null,
+                error: "Please fill out the form completely"
+            });
+            console.log('Incomplete');
+            return;
+        }
+        if (password === password_confirm) {
+            response.render('pending'),
+            usersToAdd.push({nonce:nonce, username:username, password:password, email:email});
+        } else {
+            response.render('index', {
+                user: null,
+                error: "Not sure what happened. Try again."
+            })
+        }
+    });
+    // create reusable transporter object using SMTP transport 
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'curt.poff@gmail.com',
+            pass: 'f@tback2069'
+        }
+    });
+    // NB! No need to recreate the transporter object. You can use 
+    // the same transporter object for all e-mails 
+    // setup e-mail data with unicode symbols 
+    var mailOptions = {
+        from: 'Curt Poff ✔ <curt.poff@gmail.com>', // sender address 
+        to: email, // list of receivers 
+        subject: 'Hello ✔', // Subject line 
+        text: 'Get some ✔', // plaintext body 
+        html: '<a href="http://localhost:3000/verify_email/' +
+            nonce + '">Get some!</a>' // html body 
+    };
+    // send mail with defined transport object 
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email is on the way: ' + info.response);
+        }
+    });
 });
+
+// --------------------
+// VERIFICATION
+// --------------------
+router.get('/verify_email/:nonce', function(request, response) {
+    database = app.get('database');
+    var returnedNonce = request.params.nonce;
+    console.log(usersToAdd);
+    // iterate through usersToAdd array and add user to db if nonce match is found
+    usersToAdd.forEach(function(user) {
+        if (user.nonce === returnedNonce) {
+            database('users').insert({
+                username: user.username,
+                password: user.password
+            }).then(function() {
+                response.cookie('username',
+                    user.username)
+                response.redirect('/');
+            })
+        }
+    })
+});
+
+// add new route for validate user (GET)
+// router.get('/verify_email/:nonce', function(request, response) {
 // --------------------
 // LOGIN
 // --------------------
@@ -120,33 +138,29 @@ router.post('/login', function(request, response) {
     var username = request.body.username,
         password = request.body.password,
         database = app.get('database');
-    database('users')
-        .where({
-            'username': username
-        })
-        .then(function(records) {
-            if (records.length === 0) {
+    database('users').where({
+        'username': username
+    }).then(function(records) {
+        if (records.length === 0) {
+            response.render('index', {
+                title: "You don't exist!",
+                user: null,
+                error: "No such user"
+            });
+        } else {
+            var user = records[0];
+            if (user.password === password) {
+                response.cookie('username', username);
+                response.redirect('/');
+            } else {
                 response.render('index', {
-                    title: "You don't exist!",
+                    title: 'ERROR!',
                     user: null,
-                    error: "No such user"
+                    error: "Learn how to type, jackass!"
                 });
             }
-            else {
-                var user = records[0];
-                if (user.password === password) {
-                    response.cookie('username', username);
-                    response.redirect('/');
-                }
-                else {
-                    response.render('index', {
-                        title: 'ERROR!',
-                        user: null,
-                        error: "Learn how to type, you jackass!"
-                    });
-                }
-            }
-        });
+        }
+    });
 });
 // --------------------
 // SEND TWEETS
@@ -155,14 +169,12 @@ router.post('/sendtweet', function(request, response) {
     var tweet = request.body.tweet,
         tweeter = request.cookies.username,
         database = app.get('database');
-    database('tweets')
-        .insert({
-            twit: tweet,
-            username: tweeter,
-            dateTime: new Date(Date.now())
-        })
-        .then(function() {
-            response.redirect('/');
-        });
+    database('tweets').insert({
+        twit: tweet,
+        username: tweeter,
+        dateTime: new Date(Date.now())
+    }).then(function() {
+        response.redirect('/');
+    });
 });
 module.exports = router;
