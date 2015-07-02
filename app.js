8c216f7bf7b8a72b853ee876c82f8b6815f5b020
cpoff <curt.poff@gmail.com>
2015-07-02 11:15:23 -0700

    Added port to app server

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var users = require('./routes/users');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', (process.env.PORT || 5000));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
